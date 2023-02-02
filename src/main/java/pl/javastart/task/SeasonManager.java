2efb39db7b9ba70f4dcf8af68e0e5b3e45fa48c8
Mateusz Gałecki <m.galecki10@gmail.com>
2023-02-02 19:43:27 +0100

    v0.2

--- src/main/java/pl/javastart/task/SeasonManager.java
+++ src/main/java/pl/javastart/task/SeasonManager.java
@@ -6,31 +6,27 @@ public class SeasonManager {
 
     // uzupełnij metodę - do wczytywania danych użyj przekazany Scanner
     public void run(Scanner scanner) {
-        Map<String, List> seasonsAndMonths = createSeasonsAndMonths();
         Season[] seasons = Season.values();
 
-        String userSeason = showQuestion(scanner, seasons);
-        for (Season season : seasons) {
-            if (userSeason.equals(season.getPlName())) {
-                List result = seasonsAndMonths.get(season.getPlName());
-                System.out.println("W tej porze roku są następujące miesiące:");
-                System.out.println(result);
+        Season season = showQuestion(scanner);
+        if (season != null) {
+            String[] months = season.getMonths();
+            System.out.println("W tej porze roku są następujące miesiące:");
+            for (String month : months) {
+                System.out.println(month);
             }
+        } else {
+            System.out.println("Pora roku jest nieprawidłowa");
         }
     }
 
-    private String showQuestion(Scanner scanner, Season[] seasons) {
+    private Season showQuestion(Scanner scanner) {
         System.out.println("Podaj porę roku");
+        Season[] seasons = Season.values();
         for (Season season : seasons) {
             System.out.println(season.getPlName());
         }
-        return scanner.nextLine();
-    }
-
-    private static final Map<String, List> createSeasonsAndMonths() {
-        return Map.of("Zima", List.of("grudzień", "styczeń", "luty"),
-                "Wiosna", List.of("marzec", "kwiecień", "maj"),
-                "Lato", List.of("czerwiec", "lipiec", "sierpien"),
-                "Jesień", List.of("wrzesień", "październik", "listopad"));
+        String seasonFromUser = scanner.nextLine();
+        return Season.createFromPlName(seasonFromUser);
     }
 }
